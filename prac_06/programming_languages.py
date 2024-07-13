5f32452e3abeb09c2cba0a7cdc23e61c727cec38
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-07-13 23:05:43 +1000

    added and completed languages.py & programming_languages.py added README.md

---
+++ prac_06/programming_languages.py
@@ -0,0 +1,18 @@
+class ProgrammingLanguages:
+    """Divide programming languages into parts by their nane, typing method, reflection and year of creation"""
+
+    def __init__(self, name="", typing="", reflection="", year=0):
+        """Applies a variable to each part of ProgramingLanguage class"""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Check whether language uses dynamic typing"""
+        return self.typing == "Dynamic"
+
+    def __str__(self):
+        """Returns a string representation of the ProgrammingLanguages class"""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+

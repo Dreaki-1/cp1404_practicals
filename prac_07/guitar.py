33e24fa13969dd3dd9c087a1eda1c95fd753c00a
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-07-21 00:45:06 +1000

    Part 1 of guitars complete

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,5 +1,5 @@
 """
-Prac 06 - guitar
+Prac 06 now prac 07 - guitar
 
 class Guitar
 
@@ -23,6 +23,29 @@ class Guitar:
         """Determines if guitar is vintage based on current age"""
         return self.get_age() >= VINTAGE_AGE
 
-    def __str__(self):
+    def __lt__(self, other):
+        return self.year < other.year
+
+    def csv_convert(self):
+        return [self.name, self.year, self.cost]
+
+    def __repr__(self):
         """Returns string representation of Guitar"""
         return f"{self.name} ({self.year}) ${self.cost}"
+
+
+
+# def run_test():
+#     """Runs tests"""
+#
+#     guitar1 = Guitar("gibson", 2000, 5000)
+#     guitar2 = Guitar("Alexandra 92", 1800, 11050)
+#     guitar3 = Guitar("Brazil", 1900, 2000)
+#
+#     guitars= [guitar1, guitar2, guitar3]
+#     sorted_guitars = sorted(guitars)
+#
+#     print(sorted_guitars)
+#
+#
+# run_test()

f9475db9b5d6ff9515cf5e52cd5b0db2dd5cbfe2
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-07-20 21:25:12 +1000

    Complete segments of programming languages in prac 07

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -28,7 +28,7 @@ def main():
         reflection = parts[2] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]), parts[4])
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
@@ -78,7 +78,7 @@ def using_namedtuple():
 
 def using_csv_namedtuple():
     """Language file reader version using both csv module and named tuple."""
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, pointer')
     in_file = open("languages.csv", "r")
     in_file.readline()
     for language in map(Language._make, csv.reader(in_file)):

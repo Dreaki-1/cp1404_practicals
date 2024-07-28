88967ba177012f3e65fe2ee0dfe6f1d08459c0bc
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-07-28 19:50:32 +1000

    complete dynamic_labels.kv & dynamic_labels.py prac 08

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,24 @@
+"""
+Dynamic Labels - Prac 08
+Estimated Time: 30 minutes
+Actual Time: 20 minutes
+"""
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.letter_to_number = {"A": 1, "B": 2, "C": 3, "D": 4}
+
+    def build(self):
+        self.title = "Dynamic Label"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        for key, value in self.letter_to_number.items():
+            self.root.ids.main.add_widget(Label(text=f"{key} - {value}"))
+        return self.root
+
+
+DynamicLabelsApp().run()

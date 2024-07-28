e6468fee3772b87ea7aa2d274c3cc5007b914b43
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-07-28 19:52:30 +1000

    update dynamic_labels.py prac 08

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -9,16 +9,20 @@ from kivy.uix.label import Label
 
 
 class DynamicLabelsApp(App):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.letter_to_number = {"A": 1, "B": 2, "C": 3, "D": 4}
+    """App that Displays String and Number"""
 
-    def build(self):
-        self.title = "Dynamic Label"
-        self.root = Builder.load_file('dynamic_labels.kv')
-        for key, value in self.letter_to_number.items():
-            self.root.ids.main.add_widget(Label(text=f"{key} - {value}"))
-        return self.root
+
+def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+    self.letter_to_number = {"A": 1, "B": 2, "C": 3, "D": 4}
+
+
+def build(self):
+    self.title = "Dynamic Label"
+    self.root = Builder.load_file('dynamic_labels.kv')
+    for key, value in self.letter_to_number.items():
+        self.root.ids.main.add_widget(Label(text=f"{key} - {value}"))
+    return self.root
 
 
 DynamicLabelsApp().run()

d03ea3076637d2c0b9a137a7ca055325e8631cdf
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-08-04 18:37:14 +1000

    create and begin taxi_simulator.py

---
+++ prac_09/taxi_simulator.py
@@ -0,0 +1,12 @@
+"""
+Prac 09 - Taxi Simulator
+"""
+from prac_09.car import Car
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+MENU_OPTIONS = "q, c, d"
+
+
+def main():
+

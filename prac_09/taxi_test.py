020f637912883bf61a59093c3fd032ab6baef2a2
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-08-04 14:28:46 +1000

    taxi.py passed testing

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -7,13 +7,14 @@ from taxi import Taxi
 
 
 def run_test():
-    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi = Taxi("Prius 1", 100)
     my_taxi.drive(40)
     print(my_taxi)
     my_taxi.start_fare()
     my_taxi.drive(100)
     print(my_taxi)
-
+    taxi_price = Taxi.price_per_km
+    print(taxi_price)
 
 
 

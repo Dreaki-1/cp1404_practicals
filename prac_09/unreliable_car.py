5024faabec61dec3187aa2efccf0e4359afb257c
Dreaki <dreaki.lui@my.jcu.edu.au>
2024-08-04 18:12:16 +1000

    complete silver_service_taxi.py & silver_service_taxi_test.py file
    Add docstrings to unreliable_car.py

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -9,10 +9,12 @@ from random import randint
 
 class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
+        """Initialize of  UnreliableCar"""
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
+        """Drives the car  based on reliability"""
         reliability_chance = randint(1, 100)
         if reliability_chance > self.reliability + 1:
             distance = 0
